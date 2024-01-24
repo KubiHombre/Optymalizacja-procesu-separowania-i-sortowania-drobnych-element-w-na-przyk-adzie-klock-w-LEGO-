9195665dbe9c5dd99f973522c1d2dba22ee00910
Assassin-PL <80000683+Assassin-PL@users.noreply.github.com>
2024-01-24 23:01:25 +0100

    Aktualizacja na odbiór projektu sterowanie_silnikami.ino
    zmiany podczas spotkania 24.01.2024

--- sterowanie_silnikami/sterowanie_silnikami.ino
+++ sterowanie_silnikami/sterowanie_silnikami.ino
@@ -3,26 +3,20 @@
 
 // Create the servo object
 Servo myservo;
-Servo nie360;
-Servo nie360_2;
 
 // Setup section to run once
 void setup() {
   myservo.attach(5); // attach the servo to our servo object
-  nie360.attach(2);
-  nie360_2.attach(6);
-  myservo.write(90); // stop the motor
+  //myservo.write(90);
 }
 
 // Loop to keep the motor turning!
 void loop() {
-  nie360.write(0);
-  nie360_2.write(180);
-  myservo.write(180); // rotate the motor counter-clockwise
-  delay(5000); // keep rotating for 5 seconds (5000 milliseconds)
-
-  nie360.write(180);
-  nie360_2.write(0);
-  delay(5000); // keep rotating 😀
-  
+  int n = 90;
+  int p1 = 40;
+  int p2 = 140;
+  myservo.write(p1);
+  delay(2000);
+  myservo.write(p2);
+  delay(2000);
 }
